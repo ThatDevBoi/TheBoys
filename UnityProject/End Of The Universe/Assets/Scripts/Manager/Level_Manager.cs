c1300d88de9240f817f6aa7f56d3e613a9b28215
mrfroggy56 <47029700+mrfroggy56@users.noreply.github.com>
2019-10-05 17:57:05 +0100

    Fade in screen converted with restart screen
    The fade in effect didn't have the restart screen, so i implemented this together.

--- UnityProject/End Of The Universe/Assets/Scripts/Manager/Level_Manager.cs
+++ UnityProject/End Of The Universe/Assets/Scripts/Manager/Level_Manager.cs
@@ -8,20 +8,25 @@ public class Level_Manager : MonoBehaviour
     // UI
     public Text distanceMeter;
     public Canvas StartMenu;
-    // public Canvas restartCanvas;
+    public Canvas Restart;
+    public Image deeposcreen;
+    //Strings
+    public string gameOverMessage;
     // Ints
     int currentDistance = 1;
     // Float
     float playersDistance = 1;
     // Booleans
     public static bool gameOver = false;
+    //Gameoobjicts
+    public GameObject UIHandler;
 
 
     // Fad Effect Values
     public Texture2D fadeOutTexture;        // The Texture that will overlay the screen this can be a black image or a loading graphic
     public float fadeSpeed = 5f;        // The Fading Speed
 
-    public int drawDepth = -1000;      // the texture order in the draw hierarchy a low number means in renders on top
+    public int drawDepth = 1000;      // the texture order in the draw hierarchy a low number means in renders on top
     public float alpha = 1.0f;     // the texture alpha value between 0 and 1
     public int fadeDir = -1;       // the direction to fade in = -1 or out = 1
 
@@ -31,7 +36,11 @@ public class Level_Manager : MonoBehaviour
         // Assign the canvas variable
         StartMenu = GameObject.Find("StartMenu").GetComponent<Canvas>();
         //restartCanvas = GameObject.Find("RestartMenu").GetComponent<Canvas>();
-      
+
+        deeposcreen.color = new Color(deeposcreen.color.r, deeposcreen.color.g, deeposcreen.color.b, 0);
+        deeposcreen.enabled = false;
+        UIHandler.SetActive(false);
+
         Time.timeScale = 0;
         StartMenu.enabled = true;
 
@@ -48,7 +57,9 @@ public class Level_Manager : MonoBehaviour
         if (gameOver)
         {
             StartCoroutine(GameOver());
-            Time.timeScale = 0;
+            GameOverMessage();
+            //Time.timeScale = 0;
+            //DoFade();
         }
         
         // When the game is currently not in a play state dont call the function 
@@ -60,25 +71,63 @@ public class Level_Manager : MonoBehaviour
 
     public void StartGame()
     {
-        // Chnage the time so the game will play
+        // Change the time so the game will play
         Time.timeScale = 1;
         // Turn off the canvas
         StartMenu.enabled = false;
     }
 
-    private void OnGUI()
+    public void RestartGame(string levelName)
     {
-        if(gameOver)
-        {
-            alpha += fadeDir * fadeSpeed * Time.deltaTime;      // Fade in/out the alpha value using  direction, a speed and time.delta to convert the operation to seconds
+        //Reloads the scene
+        Application.LoadLevel(levelName);
+        //Turn off the canvas
+        Restart.enabled = false;
+        gameOver = false;
+    }
 
-            alpha = Mathf.Clamp01(alpha);       // Force (clamp) the number between 0 and 1 because GUI color uses alpha vlues getween 0 and 1 
-                                                // set color of our GUI (in this case our Texture).All color values remain the same & the Apla is set to the alpha
-            GUI.color = new Color(GUI.color.r, GUI.color.g, GUI.color.b, alpha);        // Set the alpha values
-            GUI.depth = drawDepth;      // Make the black Texture render on top (drawn last)
-            GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), fadeOutTexture);
+    public void EndApplication()
+    {
+        EndApplication();
+    }
+
+    public void GameOverMessage()
+    {
+        Text gmOverText = GameObject.Find("Game_Over").GetComponent<Text>();
+        gmOverText.text = gameOverMessage.ToString();
+        if (gameOver)
+        {
+            //gameOverMessage.Length = Random.Range()
         }
     }
+
+    void DoFade()
+    {
+        alpha = deeposcreen.color.a;
+        deeposcreen.enabled = true;
+        alpha += 5 * Time.deltaTime;
+        deeposcreen.color = new Color(GUI.color.r,GUI.color.g,GUI.color.b, alpha);
+        //deeposcreen.CrossFadeAlpha(alpha += increaseAlpha, fadeDir, true);
+
+    }
+
+
+
+    //private void OnGUI()
+    //{
+    //    if(gameOver)
+    //    {
+    //        //alpha += fadeDir * fadeSpeed * Time.deltaTime;      // Fade in/out the alpha value using  direction, a speed and time.delta to convert the operation to seconds
+
+    //        //alpha = Mathf.Clamp01(alpha);       // Force (clamp) the number between 0 and 1 because GUI color uses alpha values getween 0 and 1 
+    //        //                                    // set color of our GUI (in this case our Texture).All color values remain the same & the Apla is set to the alpha
+    //        //GUI.color = new Color(GUI.color.r, GUI.color.g, GUI.color.b, alpha);        // Set the alpha values
+    //        ////GUI.depth = drawDepth;      // Make the black Texture render on top (drawn last)
+    //        //GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), fadeOutTexture);
+    //        //GUI.depth = -1;
+
+    //    }
+    //}
 
 
     public IEnumerator DistanceCalulator()
@@ -93,16 +142,22 @@ public class Level_Manager : MonoBehaviour
         yield break;
     }
 
-    public float StartFade(int direction)
-    {
-        fadeDir = direction;
-        return (fadeSpeed);
-    }
+
 
     public IEnumerator GameOver()
     {
-        yield return new WaitForSeconds(.5f);
-        StartFade(1);
+        yield return new WaitForSeconds(.1f);
+        if (deeposcreen.color.a < 255)
+        {
+            DoFade();
+        }
+        else
+            yield break;
+
+        yield return new WaitForSeconds(.2f);
+        Restart.enabled = true;
+        UIHandler.SetActive(true);
+        Time.timeScale = 0;
         yield break;
     }
 
