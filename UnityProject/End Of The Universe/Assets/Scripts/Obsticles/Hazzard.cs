6aa952a1fd9f8fb7ca8cc76dedb39f378554fb61
Dipo <mstrdipo@gmail.com>
2019-10-07 14:47:46 +0100

    Alpha push
    fixes

--- UnityProject/End Of The Universe/Assets/Scripts/Obsticles/Hazzard.cs
+++ UnityProject/End Of The Universe/Assets/Scripts/Obsticles/Hazzard.cs
@@ -6,11 +6,21 @@ public class Hazzard : MonoBehaviour
 {
     // Speed Value
     public float speed = 4;
-
+    public GameObject PC;
+    public int x;
+    private void Start()
+    {
+        x = 7;
+    }
     private void Update()
     {
         // Move gameObject left depending on the screen and Time
         transform.Translate(Vector3.left * speed * Time.deltaTime);
+
+        // distance
+        float distance = PC.transform.position.x - gameObject.transform.position.x;
+        if (Mathf.Abs(distance) > x)
+            Destroy(gameObject);
     }
 
     // Kill Object Off screen
