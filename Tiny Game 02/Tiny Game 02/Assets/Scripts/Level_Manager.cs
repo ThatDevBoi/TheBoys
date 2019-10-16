14842142c5e1d3d579dfe20ac45aef351a75a96d
mrfroggy56 <47029700+mrfroggy56@users.noreply.github.com>
2019-10-16 22:52:53 +0100

    UI
    I have added in the start menu, pause menu and dialogue boxes all as prefabs.

---
+++ Tiny Game 02/Tiny Game 02/Assets/Scripts/Level_Manager.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Level_Manager : MonoBehaviour
+{
+    // Variables
+    public Canvas StartMenu;
+    public Canvas pauseMenu;
+    
+    
+
+
+
+
+    public void Awake()
+    {
+        StartMenu = GameObject.Find("StartMenu").GetComponent<Canvas>();    // Find the start canvas in the scene
+        pauseMenu = GameObject.Find("PauseMenu").GetComponent<Canvas>();    // Find the Pause menu canvas in the scene
+
+        Time.timeScale = 0; // Pause the game while start menu is on screen
+        pauseMenu.enabled = false;  // Turn off the pause menu as the game starts
+        StartMenu.enabled = true;   // Turn on the start menu
+
+        // Debug
+        if (StartMenu == null)
+            Debug.LogWarning("You Have Not Applied The Relevant Canvas");
+        else
+            return;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Fade();
+
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            PauseMenu();
+        }
+        // Out of bounds check will go here
+    }
+
+    public void StartGame()
+    {
+        // Change the time so the game will play
+        Time.timeScale = 1;
+        // Turn off the canvas
+        StartMenu.enabled = false;
+    }
+
+    public void RestartGame(string levelName)
+    {
+
+        //Reloads the scene
+        Application.LoadLevel(levelName);   // For use with out of bounds
+        
+    }
+
+    public void UnpauseGame()
+    {
+        // Change the time so the game will play
+        Time.timeScale = 1;
+        // Turn off the canvas
+        pauseMenu.enabled = false;
+    }
+
+    public void EndApplication()
+    {
+        EndApplication();
+    }
+
+    //public void Fade()
+    //{
+    //    float alpha = -1f;  // Value that changes the density of a 2d image rendered on screen (example; the alpha colour of an image)
+    //    Texture2D fadeOutTexture;   // The image we will be manipulating
+    //    float fadeSpeed = 5f;   // How fast we want the image to change colour
+    //    int drawDepth = 1000;   // Order of render
+    //    int fadeDir = -1;   // Fade in or out, -1 in, 1 out
+
+    //    fadeOutTexture = Resources.Load<Texture2D>("Assets/blue") as Texture2D;
+
+
+
+    //    if (fadeOutTexture == null)
+    //    {
+    //        Debug.Log("No texture loaded");
+    //    }
+    //}
+
+    public void PauseMenu()
+    {
+        // Change the time so the game will pause
+        Time.timeScale = 0;
+        // Turn on the canvas
+        pauseMenu.enabled = true;
+    }
+
+}
---
+++ Tiny Game 02/Tiny Game 02/Assets/Scripts/UI_Talk.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_Talk : MonoBehaviour
+{
+    // Variables
+    public Transform playerPosition;
+    public string objectiveName;
+    public string[] chatText;
+    public int arrayLength;
+    public int stringChanger = 0;
+    public float distanceToTalk;
+    public Text dialogueText;
+
+    public GameObject DialogueCan;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        arrayLength = chatText.Length - 1;
+        DialogueCan = GameObject.Find("DialogueCanvas");
+        DialogueCan.SetActive(false);
+        gameObject.name = objectiveName;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Find the players position
+        playerPosition = GameObject.Find("Player").GetComponent<Transform>();
+        Interaction();
+        
+        if (stringChanger > chatText.Length)
+        {
+            stringChanger = 0;
+        }
+
+    }
+
+    void Interaction()
+    {
+        // Distance
+        float distance;
+        distance = Vector3.Distance(transform.position, playerPosition.position);
+        Debug.Log(distance);
+        if (distance > distanceToTalk)
+        {
+            StartCoroutine(Talk());
+        }
+    }
+
+    IEnumerator Talk()
+    {
+        DialogueCan.SetActive(true);
+        int arrayChanger = chatText[stringChanger].Length;
+        // Text Scroller
+        dialogueText.text = chatText[stringChanger];
+        // Change Sentence
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            stringChanger++;
+        }
+        // Scroller Check
+        else if (stringChanger >= arrayLength)
+        {
+            yield return new WaitForSeconds(0.50f);
+            stringChanger = 0;
+        }
+        yield break;
+    }
+}
