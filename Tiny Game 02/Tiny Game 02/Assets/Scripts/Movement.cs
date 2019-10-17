14e420842bb333aa052d3f6343605da2bc190ff6
KathrynFrost <47030048+KathrynFrost@users.noreply.github.com>
2019-10-17 20:43:17 +0100

    Smash

--- Tiny Game 02/Tiny Game 02/Assets/Scripts/Movement.cs
+++ Tiny Game 02/Tiny Game 02/Assets/Scripts/Movement.cs
@@ -18,25 +18,29 @@ public class Movement : MonoBehaviour
     private void Update()
     {
 
+        Move();
+     
+    }
 
+    private void Move()
+    {
         float v = Input.GetAxis("Vertical");
 
-       float h = Input.GetAxis("Horizontal");
+        float h = Input.GetAxis("Horizontal");
         if (Input.GetKey(KeyCode.Space))
         {
             rigidbody.AddRelativeForce(Vector3.up * 10);
             Debug.Log("y");
         }
 
-       if(h!=0)
-            rigidbody.AddRelativeTorque(0,h* F*0.01f,0);
-            Debug.Log("x");
-        
+        if (h != 0)
+            rigidbody.AddRelativeTorque(0, h * F * 0.01f, 0);
+        Debug.Log("x");
+
         if (v != 0)
         {
-            rigidbody.AddRelativeForce(0,0, v * F);
+            rigidbody.AddRelativeForce(0, 0, v * F);
             Debug.Log("x");
         }
     }
-
 }
