77639624805b7c4caa024669580e2d7c339d5962
David Brooks <davebrooks199909@gmail.com>
2019-10-20 00:09:41 +0100

    Just setting uo the player again
    I had to pull some work done by peers so i just reset the player audio on the prefab

--- Tiny Game 02/Tiny Game 02/Assets/Scripts/Audio Manager/AudioManager.cs
+++ Tiny Game 02/Tiny Game 02/Assets/Scripts/Audio Manager/AudioManager.cs
@@ -4,30 +4,40 @@ using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
+    #region KeyNote Before Alteration
+    // These values i have set work best in my opinion from testing them during runtime.
+    // Edit them but remember that you could risk breaking the immersion of an underwater environment
+    // Also remember that the audio is controlled in groups from the master audio mixer
+    // You might want to edit the volume control if you play around in this script
+    #endregion
+
+    #region Variables
     // This is for the whale noise
     public AudioSource PC_AD;
 
-    public float fadTime = 5f;
+    private float fadTime = 5f;
 
-    public float minTimeOff = 5f;
-    public float maxTimeOff = 15f;
+    private float minTimeOff = 5f;
+    private float maxTimeOff = 15f;
     float timeOff;
 
-    public float minTimeOn = 5f;
-    public float maxTimeOn = 15f;
+    private float minTimeOn = 5f;
+    private float maxTimeOn = 15f;
     float timeOn;
 
-    public float minVol = 0.5f;
-    public float maxVol = 1;
+    private float minVol = 0.5f;
+    private float maxVol = 1;
     float whaleVol;
 
-    public float timer;
-    public float timeTillNextEvent;
+    private float timer;
+    private float timeTillNextEvent;
 
-    bool whaleCalling;
+    public bool whaleNotCalling;
+    #endregion
     // Start is called before the first frame update
     void Start()
     {
+        PC_AD.volume = 0;
         RandomiseValues();
         timeTillNextEvent = timeOff;
     }
@@ -35,29 +45,36 @@ public class AudioManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Count up the timer we need this value to meet another
         timer += Time.deltaTime;
+        // When the timer is greater than the randomized value
         if(timer > timeTillNextEvent)
         {
-            if(whaleCalling)
+            // Is our whale actually calling rn?
+            if(whaleNotCalling)    // No. Lets tell the game that
             {
+                // turn down the audio from the while loop so we cannot hear the whale anymore
                 StartCoroutine(FadeWhale(PC_AD.volume, 0, fadTime));
-                whaleCalling = false;
-                RandomiseValues();
-                timeTillNextEvent = timeOff + fadTime;
+                whaleNotCalling = false;   // We are rather fading back to no noise or there was never noise
+                RandomiseValues();  // Make a new value so the timer can meet the new timeTillNextEvent Time
+                timeTillNextEvent = timeOff + fadTime;  // Set the new randomized Value
             }
-            else if (!whaleCalling)
+            else if (!whaleNotCalling) // if the whale is ready to call
             {
+                // Fade in the whale noise slowly
                 StartCoroutine(FadeWhale(0, whaleVol, fadTime));
-                whaleCalling = true;
-                RandomiseValues();
+                whaleNotCalling = true; // the whale is making noise so let the boolean take control
+                RandomiseValues();  // Make new values so we can fade out when the new value is met
                 timeTillNextEvent = timeOn + fadTime;
             }
+            // reset the timer (If we dont do this then the script will break cuz the value keeps increasing)
             timer = 0;
         }
     }
     // Random values for audio sources
     void RandomiseValues()
     {
+        // Set random values so we can call them whenever we fade the audio in and out
         timeOff = Random.Range(minTimeOff, maxTimeOff);
         timeOn = Random.Range(minTimeOn, maxTimeOn);
         whaleVol = Random.Range(minVol, maxVol);
@@ -66,7 +83,7 @@ public class AudioManager : MonoBehaviour
     IEnumerator FadeWhale(float startValue, float endValue, float duration)
     {
         float currentTIme = 0;
-
+        // Slowly increase the volume from the audio source variable
         while(currentTIme <= duration)
         {
             PC_AD.volume = Mathf.Lerp(startValue, endValue, (currentTIme / duration));
