8d548da2ab59044c45efbe5fa8ecbb60872f3803
ThatDevBoi <davebrooks199909@gmail.com>
2019-10-21 17:54:05 +0100

    School of fush
    A school of fih that have random animation speeds and move lin circular motions

---
+++ Tiny Game 02/Tiny Game 02/Assets/FishSchool_Movement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishSchool_Movement : MonoBehaviour
+{
+    public float speed = 2;
+    float timeCounter = 0;
+    float width;
+    float height;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        width = 4;
+        height = 2;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeCounter += Time.deltaTime * speed;
+        float x = Mathf.Cos(timeCounter) * width;
+        float y = Mathf.Sin(timeCounter) * height;
+        float z = 2;
+        transform.position += new Vector3(x, y, transform.forward.z * z);
+    }
+}
---
+++ Tiny Game 02/Tiny Game 02/Assets/changeAnimSpeed.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class changeAnimSpeed : MonoBehaviour
+{
+    float speedDifference = 0;
+    float minSpeed = .1f;
+    float maxSpeed = 1;
+    public Animator fishAnim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        fishAnim = GetComponent<Animator>();
+
+        speedDifference = Random.Range(minSpeed, maxSpeed);
+        fishAnim.speed = speedDifference;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
