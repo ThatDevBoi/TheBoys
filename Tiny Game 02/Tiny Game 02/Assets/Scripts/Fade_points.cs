4e57b07838a22d7f90b2f2a274ea8431a8f842d0
Dipo <mstrdipo@gmail.com>
2019-10-21 17:31:41 +0100

    fog_fade

---
+++ Tiny Game 02/Tiny Game 02/Assets/Scripts/Fade_points.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fade_points : MonoBehaviour
+{
+    
+    private Level_Manager get_score;
+    private float fade_percentage;
+    // Start is called before the first frame update
+    void Start()
+    {
+        get_score=GetComponent<Level_Manager>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        fade_percentage = get_score.trashScore;
+        RenderSettings.fogDensity = 0.01f - (fade_percentage/100);        
+    }
+}
---
+++ Tiny Game 02/Tiny Game 02/Assets/Scripts/Fish_school.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fish_school : MonoBehaviour
+{
+    public GameObject base_fish;
+    public int quantity;
+    private int i;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (i = 0; i <= quantity; i++)
+            Instantiate(base_fish);
+    }
+}
