f929cb7b0cb460c2f684fe2ada013d8080b7239f
David Brooks <davebrooks199909@gmail.com>
2019-12-19 16:00:19 +0000

    Level Manager
    In this commit i deicded to make a level manager which would be handy later in the game. The level manager will be used to balance the game and contain information which can cause restrictions with abilities, also containing how many objects are in one scene so we can play around and turn them off to save CPU process. The Level manager also has a spawner to just check if there is a level mnager already in the scene. if the level manager is not in  the scene then it will spawn it for us

---
+++ Game 3/Gam3_UnityProject/Assets/Scripts/GameManager/Spawn_Level_Manager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn_Level_Manager : MonoBehaviour
+{
+
+    public GameObject LevelManagerPrefab;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if(!FindManager())
+        {
+            LevelManagerPrefab = Resources.Load<GameObject>("Level_Manager/LevelManager");
+            GameObject currentObject = this.gameObject;
+            Instantiate(LevelManagerPrefab, transform.position, Quaternion.identity);
+            Destroy(currentObject);
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    bool FindManager()
+    {
+        if (GameObject.Find("Level_Manager") == null)
+        {
+            // not in the scene
+            return false;
+        }
+        else
+            // in the scene
+            return true;
+    }
+}
