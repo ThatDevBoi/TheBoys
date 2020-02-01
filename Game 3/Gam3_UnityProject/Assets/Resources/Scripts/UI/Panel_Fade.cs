96f3af801c1dcea3dbadb6cd1d42da108e57eb8f
Dipo <mstrdipo@gmail.com>
2020-02-01 20:52:19 +0000

    Panel with exposition now is invisible
    text is not transparent, will be fixed

---
+++ Game 3/Gam3_UnityProject/Assets/Resources/Scripts/UI/Panel_Fade.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Panel_Fade : MonoBehaviour
+{
+    private GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("PC");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player.GetComponent<Player_Controller>().lastVelocity != Vector3.zero)
+        {
+            GetComponent<CanvasRenderer>().SetAlpha(0);
+            GetComponentInChildren<MeshRenderer>().material.color*=new Vector4(1,1,1,0);
+        }
+        else
+            GetComponent<CanvasRenderer>().SetAlpha(1);
+    }
+
+   
+}
