dd12d889cabba2276f54ff1b64b05905c6d4056f
Dipo Master <mstrdipo@gmail.com>
2020-02-06 19:31:33 +0000

    less intrusive exposition panel
    the exposition panel now appears less frequently

--- Game 3/Gam3_UnityProject/Assets/Resources/Scripts/UI/Panel_Fade.cs
+++ Game 3/Gam3_UnityProject/Assets/Resources/Scripts/UI/Panel_Fade.cs
@@ -2,10 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
-
+using System;
 public class Panel_Fade : MonoBehaviour
 {
     private GameObject player;
+    private TextMeshPro storeText;
+    private float stoppedMoving;
+    public bool dialouge;
+    private Color32 fade=Color.clear;
+  // private bool reading=false;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,18 +20,32 @@ public class Panel_Fade : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // When the players Rigidbody Velocity is not moving
-        if (player.GetComponent<Player_Controller>().playerPhysics.velocity != Vector3.zero)
-        {
-            GetComponent<CanvasRenderer>().SetAlpha(0);
-            GetComponentInChildren<TextMeshPro>().faceColor = new Color32(1, 1, 1, 0);
-        }
-        else
-        {
-            GetComponent<CanvasRenderer>().SetAlpha(255);
-            GetComponentInChildren<TextMeshPro>().faceColor = new Color32(255, 255, 255, 255);
-        }
-    }
 
+        
+            
+            
+                
+           
+         if (player.GetComponent<Player_Controller>().playerPhysics.velocity != Vector3.zero|| Input.GetButtonDown("Fire1"))// When the players Rigidbody Velocity is not moving
+            {
+         GetComponent<CanvasRenderer>().SetAlpha(0);
+            GetComponentInChildren<TextMeshPro>().faceColor = new Color32(1, 1, 1, 0);//hide the text panel
+                                                                                      //storeText.text= GetComponentInChildren<TextMeshPro>().text;
+            stoppedMoving = Time.time;
+        }
+        else  if(Time.time- stoppedMoving>=5)
+            {
+            
+            //timer += Time.deltaTime / 10;
+            fade = Color32.Lerp(fade, Color.white, Time.time / 100);
+            GetComponent<CanvasRenderer>().SetAlpha(fade.a);
+            GetComponentInChildren<TextMeshPro>().faceColor = fade;
+        }
+
+
+
+
+}
+    
    
 }
