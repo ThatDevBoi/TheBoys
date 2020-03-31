c6a4fda2d24fc7d6c96dcd5e584a80977afe3b30
David Brooks <davebrooks199909@gmail.com>
2020-03-31 15:14:47 +0100

    Mini Boss Begins
    I allowed the mini boss to move and has a simple shoot. Currently its hard to move forward with this mini boss class as i know very little about core functionality that the team want for their levels. Next i will add health to the Mini Boss and better shooting

---
+++ Game 3/Gam3_UnityProject/Assets/Mini_Boss_AI.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mini_Boss_AI : MonoBehaviour
+{
+    [LabelArray(new string[] { "Dipo", "Katie", "Nadeem"})]
+    public bool[] whatBoss_;
+
+    [Header("Variables Dipo")]
+    public float speed;
+    public int damage;
+    public int currentHealth;
+    private int maxHealth = 100;
+
+    private Vector3 moveDirection;
+    private Quaternion currentRotation;
+    // Shooting
+    public float ammo;
+    public LayerMask whatToShoot;
+    public float weaponRange;
+    public float fireRate;
+    public float fireRateTimer;
+    [SerializeField]
+    private Transform target;
+    private RaycastHit hit;
+
+
+
+
+    //[Header("Katie")]
+
+
+    //[Header("Nadeem")]
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = GameObject.Find("PC").GetComponent<Transform>();
+        // Set the health
+        currentHealth = maxHealth;
+        //
+        
+        //
+
+        //
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Fire Rate for gun
+        if (fireRateTimer < fireRate)
+            fireRateTimer += Time.deltaTime;
+
+        if (whatBoss_[0] == true)
+        {
+            // Functions
+            movement();
+            Shoot();
+        }
+    }
+
+    void movement()
+    {
+        if(Vector3.Distance(transform.position, target.position) >= 18)
+        {
+            speed = 2;
+            transform.position = Vector3.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+        }
+        else if(Vector3.Distance(transform.position, target.position) <= 3)
+        {
+            speed = 0;
+        }
+    }
+
+    void Shoot()
+    {
+        if (fireRateTimer < fireRate) return;
+        fireRateTimer = 0;
+        if (Physics.Raycast(transform.position, transform.TransformDirection(transform.forward), out hit, weaponRange, whatToShoot))
+        {
+            Player_Controller hitDamage = target.gameObject.GetComponent<Player_Controller>();
+            hitDamage.HitDetection(gameObject.GetComponent<Transform>());
+            hitDamage.ApplyDamage(damage);
+            Debug.DrawRay(transform.position, transform.forward * hit.distance, Color.blue);
+        }
+    }
+
+    void Health()
+    {
+
+    }
+}
